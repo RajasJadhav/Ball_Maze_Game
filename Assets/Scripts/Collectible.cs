e302a377abae8e2f0be05cc2b2acfbd3ba374928
RajasJadhav <rajas.jadhav2604@gmail.com>
2026-03-22 17:47:06 +0530

    added obstacles and collectibles

---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+
+    [SerializeField] private float rotationSpeed = 20f;
+
+    private void Update()
+    {
+        RotateAround();
+    }
+
+    private void RotateAround()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime , Space.Self);
+    }
+    
+}
