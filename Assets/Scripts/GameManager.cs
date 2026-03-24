13d855c1bfd607ffd60c3e23033d750387c2113a
RajasJadhav <rajas.jadhav2604@gmail.com>
2026-03-25 00:49:43 +0530

    added time and title screens

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,74 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timeText;
+    public Button startButton;
+
+    public GameObject titleScreen;
+    public GameObject gameOverScreen;
+
     private int score;
+    private float time;
+    public bool isGameActive;
 
     private void Start()
     {
-        score = 0;
-        UpdateScore(0);
+        
 
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+    }
+
+    public void StartGame()
+    {
+        score = 0;
+        time = 60;
+        isGameActive = true;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
     }
 
     public void UpdateScore(int scoreToAdd)
     {
        score += scoreToAdd;
        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateTime()
+    {
+        if(isGameActive)
+        {
+            if (time > 0)
+            {
+                time -= Time.deltaTime;
+            }
+            timeText.text = "Time: " + Mathf.FloorToInt(time).ToString();
+            if (time == 0)
+            {
+                GameOver();
+            }
+        }
+            
+        
+    } 
+    
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverScreen.SetActive(true);
+
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
